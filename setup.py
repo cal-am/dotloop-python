394347049567fd683eaef98bd95c446d2b534eb7
benr-calam <benr@cal-am.com>
2020-08-10 14:22:14 -0700

    Update version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name='dotloop-python',
-        version='0.0.2',
+        version='0.0.3',
         packages=setuptools.find_packages(),
         install_requires=['requests'],
         author='Ben Russell',
