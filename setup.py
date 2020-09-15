b0b39ff65d3ce76abb63d152e4b616249166743b
Ben Russell <65563736+benr-calam@users.noreply.github.com>
2020-09-15 09:50:15 -0700

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name='dotloop-python',
-        version='0.0.5',
+        version='0.0.6',
         packages=setuptools.find_packages(),
         install_requires=['requests'],
         author='Ben Russell',
